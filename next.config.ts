f1514803a0161a9344edb2c8e142d562819f3421
Noah <nnnoahhhill@gmail.com>
2026-02-01 16:33:46 -0800

    yuh

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  experimental: {
-    serverComponentsExternalPackages: ["@prisma/client"],
-  },
+  serverExternalPackages: ["@prisma/client"],
 };
 
 export default nextConfig;
